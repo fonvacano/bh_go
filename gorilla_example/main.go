c78472895c6e23a7011ff2c09958e603c9776097
Shrek <alexandrproskr@gmail.com>
2024-01-28 22:41:08 +0400

    add negroni

--- gorilla_example/main.go
+++ gorilla_example/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 	"net/http"
 )
+
+type badAuth struct {
+	user string
+	pass string
+}
+
+// do not use it, just demonstration
+func (a *badAuth) serveHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	user := r.URL.Query().Get("user")
+	pass := r.URL.Query().Get("pass")
+
+	if user != a.user || pass != a.pass {
+		http.Error(w, "auth error", 401)
+		return
+	}
+
+	ctx := context.WithValue(r.Context(), "user", user)
+	r = r.WithContext(ctx)
+	next(w, r)
+}
 
 func main() {
 	r := mux.NewRouter()
