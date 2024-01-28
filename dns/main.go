59f0eb480c2b9458c2c8788fb0188e89da0756dd
Shrek <alexandrproskr@gmail.com>
2024-01-29 01:20:08 +0400

    dns client

--- dns/main.go
+++ dns/main.go
@@ -1,17 +1,37 @@
 package main
 
-import "github.com/miekg/dns"
+import (
+	"fmt"
+	"github.com/miekg/dns"
+)
 
 func main() {
-	domains := make([]string, 1)
-	domains = append(domains, "stacktitan.com", "ya.ru")
+	domains := make([]string, 0)
+	domains = append(domains, "ya.ru", "stacktitan.com")
 
 	var msg dns.Msg
+	mp := make(map[string][]dns.RR)
 
 	for _, d := range domains {
+		mp[d] = make([]dns.RR, 0)
 		fqdn := dns.Fqdn(d)
 		msg.SetQuestion(fqdn, dns.TypeA)
-		dns.Exchange(&msg, "8.8.8.8:53")
+		exchange, err := dns.Exchange(&msg, "8.8.8.8:53")
+		if err != nil {
+			panic(err)
+		}
+		if len(exchange.Answer) < 1 {
+			fmt.Println("No Records")
+			continue
+		}
+
+		for _, answer := range exchange.Answer {
+			if a, ok := answer.(*dns.A); ok {
+				fmt.Println(a.A)
+			}
+			mp[d] = append(mp[d], answer)
+		}
 	}
+	fmt.Println(mp)
 
 }
